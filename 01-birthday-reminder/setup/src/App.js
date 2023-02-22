62b196d0f7450cd27b37c28d127913bd9864aec6
Young <ytm199891@gmail.com>
2023-02-22 17:44:55 +0800

    I don't know why my idx is not increasing.

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,38 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleMinus = () => {
+    const newPeople = [...people];
+    newPeople.pop();
+    setPeople(newPeople);
+  };
+
+  const handleAdd = () => {
+    let idx = 0;
+    const newPeople = [...people];
+    if (idx < data.length) {
+      newPeople.push(data[idx]);
+      setPeople(newPeople);
+      idx++;
+    }
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={handleAdd}>Add one</button>
+        <button onClick={handleMinus}>Minus one</button>
+        <button onClick={() => setPeople(data)}>Add people</button>
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
