1874dac7c36ae11db0e539617356f8919911cabe
Young <ytm199891@gmail.com>
2023-02-24 16:35:59 +0800

    Another project, rather just a small practice.

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ info, title }) => {
+  const [show, setShow] = useState(false);
 
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          className="btn"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          {show ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+        {show && <p>{info}</p>}
+      </header>
+    </article>
+  );
+};
 export default Question;
